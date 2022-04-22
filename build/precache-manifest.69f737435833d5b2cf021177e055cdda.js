049946ad24d36dc59d90645c884b84fafbfc43d6
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2022-04-22 13:53:04 +0400

    deploying new frontend

---
+++ build/precache-manifest.69f737435833d5b2cf021177e055cdda.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319fae05af013e842bdb3dc964235c49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d506e7f5e04db0ca99d",
+    "url": "./static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "8bbfa2a836a460f0ebf0",
+    "url": "./static/css/main.5a0501e7.chunk.css"
+  },
+  {
+    "revision": "1d506e7f5e04db0ca99d",
+    "url": "./static/js/2.4c7d45c7.chunk.js"
+  },
+  {
+    "revision": "30658350bb9ca41932af8a9bc717499c",
+    "url": "./static/js/2.4c7d45c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbfa2a836a460f0ebf0",
+    "url": "./static/js/main.4c37e9ac.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "2ababee041e660ae168a4d0b2f15e87b",
+    "url": "./static/media/image1.2ababee0.jpg"
+  },
+  {
+    "revision": "2ad6e9e78f3d2108c611e73edea4b183",
+    "url": "./static/media/image2.2ad6e9e7.jpg"
+  },
+  {
+    "revision": "98efbf0860b626ddf35f1a18fbdcbb8c",
+    "url": "./static/media/image3.98efbf08.jpg"
+  },
+  {
+    "revision": "7d6d29821a819b724e3024e7314bb33c",
+    "url": "./static/media/image4.7d6d2982.jpg"
+  },
+  {
+    "revision": "bc4c465f7db6fbe8014ec0608784a653",
+    "url": "./static/media/img1.bc4c465f.jpg"
+  },
+  {
+    "revision": "dd4cec224459aab7d58234be37f3a1ca",
+    "url": "./static/media/img2.dd4cec22.jpg"
+  },
+  {
+    "revision": "6b27b06630f88b9db3105e016f6f7e38",
+    "url": "./static/media/img3.6b27b066.jpg"
+  },
+  {
+    "revision": "4ab49220dbac6c3fdb6860e1322e6019",
+    "url": "./static/media/login.4ab49220.svg"
+  },
+  {
+    "revision": "a9b18954e6867816cd02479f290fa3eb",
+    "url": "./static/media/master.a9b18954.png"
+  },
+  {
+    "revision": "df4f0a8b3e3ed1e3c11ec88944aec0bd",
+    "url": "./static/media/register.df4f0a8b.svg"
+  },
+  {
+    "revision": "938cd7ea814419b29a149b03c60f5f3a",
+    "url": "./static/media/t-shirtimg.938cd7ea.png"
+  }
+]);
